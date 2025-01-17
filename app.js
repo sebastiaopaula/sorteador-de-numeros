9d7de928b79e034225b6c5d5a3c6f723449e29df
Sebastiao de Paula <sebastiao.paula@gmail.com>
2025-01-16 22:40:40 -0300

    Criada a function reiniciar

--- app.js
+++ app.js
@@ -8,12 +8,42 @@ function sortear(){
    
    for (let i = 0; i < quantidade; i++){
         numero = obterNumeroAleatorio(de, ate);
+        while(sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate);
+        }
         sorteados.push(numero);
    }
-   alert(sorteados);
+   let resultado=document.getElementById('resultado');
+   resultado.innerHTML=`<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+   alterarStatusBotao();
 } 
 
 
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1) )+ min;
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    let btnSortear = document.getElementById('btn-sortear');
+
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+        btnSortear.classList.remove('container__botao'); 
+        btnSortear.classList.add('container__botao-desabilitado');      
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+        btnSortear.classList.remove('container__botao-desabilitado'); 
+        btnSortear.classList.add('container__botao');      
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
 }
